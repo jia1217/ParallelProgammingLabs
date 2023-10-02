def646d2ec30e16dcb950022f1ab53d08adbe4e8
ngdxzy <zhenyu_xu@uri.edu>
2023-10-02 19:14:40 -0400

    polish_scripts

--- Labs/create_prj.sh
+++ Labs/create_prj.sh
@@ -10,42 +10,56 @@ mkdir ${PRJ_NAME}/Vitis_HLS
 mkdir ${PRJ_NAME}/Vitis_HLS/srcs
 mkdir ${PRJ_NAME}/Vitis_HLS/IP
 
-echo ".PHONY: all clean" > ${PRJ_NAME}/Vitis_HLS/Makefile
-echo "" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo "all:" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo -e "\t-@vitis_hls -f ${PRJ_NAME}_prj.tcl" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo "" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo "clean:" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo -e "\t-@rm -rf ${PRJ_NAME}" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo -e "\t-@rm -rf IP/*" >> ${PRJ_NAME}/Vitis_HLS/Makefile
-echo -e "\t-@rm -f *.log" >> ${PRJ_NAME}/Vitis_HLS/Makefile
+cat > ${PRJ_NAME}/Vitis_HLS/Makefile << EOL
+.PHONY: all clean
 
-echo "# create project" > ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "open_project -reset ${PRJ_NAME}" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "# set top function" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "set_top ${PRJ_NAME}" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "# add files" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "add_files ./srcs/${PRJ_NAME}.cpp" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "add_files ./srcs/${PRJ_NAME}.hpp" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "add_files -tb ./srcs/${PRJ_NAME}_test.cpp" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "# create solution" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "open_solution -reset -flow_target vivado solution1" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "set_part {xc7z020clg400-1}" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "create_clock -period 10 -name default" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "csynth_design" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "export_design -rtl verilog -format ip_catalog -output ./IP" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
-echo "exit" >> ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl
+all:
+    -@vitis_hls -f ${PRJ_NAME}_prj.tcl
 
-echo "#ifndef __${PRJ_NAME}_HPP__" > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.hpp
-echo "#define __${PRJ_NAME}_HPP__" >> ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.hpp
-echo "" >> ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.hpp
-echo "#endif" >> ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.hpp
-echo "#include \"${PRJ_NAME}.hpp\"" > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.cpp
-echo "#include \"${PRJ_NAME}.hpp\"" > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}_test.cpp
+clean:
+    -@rm -rf ${PRJ_NAME}
+    -@rm -rf IP/*
+    -@rm -f *.log
+
+EOL
+
+cat > ${PRJ_NAME}/Vitis_HLS/${PRJ_NAME}_prj.tcl << EOL
+# create project
+open_project -reset ${PRJ_NAME}
+
+# set top function
+set_top ${PRJ_NAME}
+
+# add files
+add_files ./srcs/${PRJ_NAME}.cpp
+add_files ./srcs/${PRJ_NAME}.hpp
+add_files -tb ./srcs/${PRJ_NAME}_test.cpp
+
+# create solution
+open_solution -reset -flow_target vivado solution1
+set_part {xc7z020clg400-1}
+create_clock -period 10 -name default
+
+csynth_design
+
+export_design -rtl verilog -format ip_catalog -output ./IP
+
+exit
+EOL
+
+cat > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.hpp << EOL
+#ifndef __${PRJ_NAME}_HPP__
+#define __${PRJ_NAME}_HPP__
+
+#endif
+EOL
+
+cat > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}.cpp << EOL
+#include "${PRJ_NAME}.hpp"
+EOL
+
+cat > ${PRJ_NAME}/Vitis_HLS/srcs/${PRJ_NAME}_test.cpp << EOL
+#include "${PRJ_NAME}.hpp"
+EOL
+
 mkdir ${PRJ_NAME}/Vivado
